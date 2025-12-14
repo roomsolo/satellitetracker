7cbdbe2eb56b07d2d504ff4962685ad4597d0a7c
roomsolo <brky.clk0@gmail.com>
2025-12-14 21:41:32 +0300

    typos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from skyfield.api import load, EarthSatellite, wgs84
 import requests
 import geocoder
+ip = geocoder.ip('me')
 ts = load.timescale()
 t = ts.now()
 def get_iss_tle():
@@ -21,11 +22,18 @@ sat = EarthSatellite(line1, line2, name, ts)
 pos = sat.at(t)
 subpoint = wgs84.subpoint(pos)
 observer = wgs84.latlon(
-    latitude_degrees=41.0082,
-    longitude_degrees=28.9784,
+    latitude_degrees=ip.lat,
+    longitude_degrees=ip.lng,
     elevation_m=30
 )
+difference = sat - observer
+topocentric = difference.at(t)
+alt, az, distance = topocentric.altaz()
 
+if alt.degrees > 0:
+    print("SATELLITE IS VISIBLE TO EYE")
+else:
+    print("SATELLITE IS NOT VISIBLE TO EYE")
 print("SATELLITE:", sat)
 print("LATITUDE:", subpoint.latitude.degrees)
 print("LONGITUDE:", subpoint.longitude.degrees)
